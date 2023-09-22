877ca3a77d5885258b622ef8062080835e6f8d3b
Mateusz Plewa <mateusz.plewa@artig.io>
2023-09-22 13:07:58 +0200

    window creation

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require('electron')
+
+
+// createWindow() function loads your web page into a new BrowserWindow instance
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 900,
+        height: 900,
+        frame: false
+    });
+
+    win.loadFile('index.html');
+};
+
+// CALLING FUNCTION WHEN THE APP IS READY
+
+app.whenReady().then(() => {
+    createWindow();
+});
